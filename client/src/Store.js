b194c738d6012d6052fd62a14f039e8ac2454ef7
DennisMalm <malm.dennis@gmail.com>
2020-08-20 21:40:45 +0200

    Added: store

--- client/src/Store.js
+++ client/src/Store.js
@@ -1,23 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
-export const RegUsernameContext = React.createContext('');
-export const RegPasswordContext = React.createContext('');
+export const UserInfoContext = React.createContext({});
 
-const Store = ({children}) => {
-    const [regUsername, setRegUsername] = useState('');
-	const [regPassword, setRegPassword] = useState('');
+const Store = ({ children }) => {
+
+	const [userInfo, setUserInfo] = useState({ username: '', password: '' });
+	return (
+		<UserInfoContext.Provider value={[userInfo, setUserInfo]}>
+			{children}
+		</UserInfoContext.Provider>
+	);
+};
+
+export default Store;
+
+/* export const RegUsernameContext = React.createContext('');
+export const RegPasswordContext = React.createContext(''); */
+
+/* const [regUsername, setRegUsername] = useState('');
+	const [regPassword, setRegPassword] = useState(''); */
 	/* const [loginUsername, setLoginUsername] = useState('');
 	const [loginPassword, setLoginPassword] = useState('');
     const [data, setData] = useState(null); */
-    
-    return (
-        <RegUsernameContext.Provider value={[regUsername, setRegUsername]}>
-            <RegPasswordContext.Provider value={[regPassword, setRegPassword]}>
-            {children}
-            </RegPasswordContext.Provider>
-        </RegUsernameContext.Provider>
-    )
-}
-
-export default Store;
