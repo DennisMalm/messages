f04b58bad39440bd476623ec92b24a3002d8d842
Dennis Malm <malm.dennis@gmail.com>
2020-08-25 17:43:24 +0200

    added card

---
+++ client/src/Components/Flow.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useRef } from 'react';
+import Card from './Card';
+import Loading from './Loading';
+import { useEffect } from 'react';
+
+function createCard(message) {
+	return (
+		<Card
+			key={message._id}
+			id={message._id}
+			name={message.name}
+			content={message.content}
+			like={message.likes}
+		/>
+	);
+}
+
+function useInterval(callback, delay) {
+	const savedCallback = useRef();
+
+	// Remember the latest callback.
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+
+	// Set up the interval.
+	useEffect(() => {
+		function tick() {
+			savedCallback.current();
+		}
+		if (delay !== null) {
+			let id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay]);
+}
+
+function Flow() {
+	const [loading, setLoading] = useState(true);
+	const [content, setContent] = useState([]);
+	const [count, setCount] = useState(0);
+
+	async function getData() {
+		const res = await fetch('/message');
+		const data = await res.json();
+		const list = data.reverse();
+		setContent(list);
+		setLoading(false);
+		console.log(list);
+	}
+
+	useInterval(() => {
+		console.log('Refresh count: ' + count);
+		setCount(count + 1);
+		getData();
+	}, 6000);
+
+	//console.log(content);
+
+	return (
+		<div>{loading || !content ? <Loading /> : content.map(createCard)}</div>
+	);
+}
+export default Flow;
